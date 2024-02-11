e17f4addad56a5fe659be01a8c217ba1cc4aa019
xClaudiaArias <xClaudiaArias@gmail.com>
2024-02-10 20:57:06 -0500

    COMPLETE -- needs styling

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # rock-paper-scissors
 Rock Paper Scissors GAME using Javascript.
+by Claudia Arias.
--- scripts.js
+++ scripts.js
@@ -1,72 +1,96 @@
+
+
+let user_choice = ""
+
+// Comp choice 
 const choices = ['rock', 'paper', 'scissors']
 const getComputerChoice = () => {
     let comp_choice = choices[Math.floor(Math.random() * choices.length)]
-    // console.log(comp_choice)
     return comp_choice
 }
 
-const playGame = () => {
-    let userScore = 0;
-    let computerScore = 0;
 
-    let i = 0;
+let playerSelection = document.querySelectorAll('.uc').forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        return e.target.value
+    })
+})
 
-    while ( i < 5) {
-        var playerSelection= prompt("Choose: Rock, Paper or Scissors","");
-        var computerSelection = getComputerChoice()
+let computerSelection = getComputerChoice()
+let updateUserScore = document.getElementById("user-score");
+let updateCompScore = document.getElementById("computer-score");
+let userScore = parseInt(updateUserScore.innerHTML);
+let computerScore = parseInt(updateCompScore.innerHTML);
+let announceWin = document.getElementById("winner-container")
+let rounds = 5
 
-        const playRound = (playerSelection, computerSelection) => {
+const playRound = (playerSelection) => {
 
+    if ((computerScore + userScore) <= rounds) {
+        rounds -= 1
+        if (computerSelection === 'rock' && playerSelection === 'rock') {
+            console.log(`${computerSelection} is the same as ${playerSelection }: Its a tie!`)
+            console.log("tie")
+            return "tie"
+        } else if (computerSelection === 'rock' && playerSelection === 'paper') {
+            console.log(`${playerSelection} beats ${computerSelection}: Player wins!`)
+            userScore += 1
+            console.log("computerScore ", computerScore , "userScore ", userScore)
+            return updateUserScore.innerHTML = userScore.toString()
+        } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
+            console.log(`${computerSelection} beats ${playerSelection}: Computer wins!`)
+            computerScore += 1
+            console.log("computerScore ", computerScore , "userScore ", userScore)
+            return updateCompScore.innerHTML  = computerScore.toString()
+        } else if (computerSelection === 'paper' && playerSelection === 'rock') {
+            console.log(`${computerSelection} beats ${playerSelection}: Computer wins!`)
+            computerScore += 1
+            console.log("computerScore ", computerScore , "userScore ", userScore)
+            return updateCompScore.innerHTML  = computerScore.toString()
+        } else if (computerSelection === 'paper' && playerSelection === 'paper') {
+            console.log(`${computerSelection} is the same as ${playerSelection }: Its a tie!`)
+            console.log("tie")
+            return "tie"
+        } else if (computerSelection === 'paper' && playerSelection == 'scissors') {
+            console.log(`${playerSelection} beats ${computerSelection}: Player wins!`)
+            userScore += 1
+            console.log("computerScore ", computerScore , "userScore ", userScore)
+            return updateUserScore.innerHTML = userScore.toString()
+        } else if (computerSelection === 'scissors' && playerSelection === 'rock') {
+            console.log(`${playerSelection} beats ${computerSelection}: Player wins!`)
+            userScore += 1
+            console.log("computerScore ", computerScore , "userScore ", userScore)
+            return updateUserScore.innerHTML  = userScore.toString()
+        } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
+            console.log(`${computerSelection} beats ${playerSelection}: Computer wins!`)
+            computerScore += 1
+            console.log("computerScore ", computerScore , "userScore ", userScore)
+            return updateCompScore.innerHTML  = computerScore.toString()
+        } else if (computerSelection === 'scissors' && playerSelection === 'scissors') {
+            console.log(`${computerSelection} is the same as ${playerSelection }: Its a tie!`)
+            console.log("tie")
+            return "tie"
+        } 
 
-            console.log("computerSelection: ", computerSelection)
-            console.log("playerSelection: ", playerSelection)
-        
-            // ROCK 
-            if (computerSelection === 'rock' && playerSelection === 'rock') {
-                console.log(`${computerSelection} is the same as ${playerSelection }: Its a tie!`)
-                console.log("tie")
-            } else if (computerSelection === 'rock' && playerSelection === 'paper') {
-                console.log(`${playerSelection} beats ${computerSelection}: Player wins!`)
-                userScore += 1
-            } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
-                console.log(`${computerSelection} beats ${playerSelection}: Computer wins!`)
-                computerScore += 1
-            } else if (computerSelection === 'paper' && playerSelection === 'rock') {
-                console.log(`${computerSelection} beats ${playerSelection}: Computer wins!`)
-                computerScore += 1
-            } else if (computerSelection === 'paper' && playerSelection === 'paper') {
-                console.log(`${computerSelection} is the same as ${playerSelection }: Its a tie!`)
-                console.log("tie")
-            } else if (computerSelection === 'paper' && playerSelection == 'scissors') {
-                console.log(`${playerSelection} beats ${computerSelection}: Player wins!`)
-                userScore += 1
-            } else if (computerSelection === 'scissors' && playerSelection === 'rock') {
-                console.log(`${playerSelection} beats ${computerSelection}: Player wins!`)
-                userScore += 1
-            } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
-                console.log(`${computerSelection} beats ${playerSelection}: Computer wins!`)
-                computerScore += 1
-            } else if (computerSelection === 'scissors' && playerSelection === 'scissors') {
-                console.log(`${computerSelection} is the same as ${playerSelection }: Its a tie!`)
-                console.log("tie")
-            }
-        }   
-
-        playRound(playerSelection, computerSelection)
-        
-        i++
-    }
-
-    console.log(userScore, computerScore, " --> scores")
-
-    if (userScore > computerScore) {
-        console.log(`User: ${userScore} Computer: ${computerScore} --- User Wins`)
-    } else if (computerScore > userScore) {
-        console.log(`User: ${userScore} Computer: ${computerScore} --- Computer Wins`)
     } else {
-        console.log(`User: ${userScore} Computer: ${computerScore} --- It's a tie`)
+        if (userScore > computerScore) {
+            announceWin.innerHTML = " User wins"
+        } else if (computerScore > userScore) {
+            announceWin.innerHTML = "Computer wins"
+        } else {
+            announceWin.innerHTML = "Its a tie :)"
+        }
+        console.log("game over")
     }
 
-}
+} 
 
-playGame()
+
+
+
+
+
+
+
+
+
